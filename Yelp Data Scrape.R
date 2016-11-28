f64a1bd5647ad8c25e9113cbaf93f85033ba5adf
jamesma618 <jamesma618@gmail.com>
2016-11-28 18:57:09 -0500

    Add files via upload
    Code for pulling data from Yelp API

---
+++ Yelp Data Scrape.R
@@ -0,0 +1,24 @@
+consumerKey = "JmaomcAzFt7hKPNfS98NvQ"
+consumerSecret = "4-ZNGbGD18oKjUSnL2n9Bth-QJo"
+token = "hpbsaWY87YYNRYpr2gSH6eVyo9mhH_ZJ"
+token_secret = "TZfgmUZq3gIx5vFmEwyaMrf_7yU"
+
+library(httr)
+library(httpuv)
+library(jsonlite)
+
+myapp = oauth_app("YELP", key=consumerKey, secret=consumerSecret)
+sig=sign_oauth1.0(myapp, token=token,token_secret=token_secret)
+
+########
+yelp_scrape <- function(term, location, limit=10) {
+  myapp <- oauth_app("YELP", key=consumerKey, secret=consumerSecret)
+  sig <- sign_oauth1.0(myapp, token=token, token_secret=token_secret)
+  
+  yelpurl <- "https://api.yelp.com/v2/search/"
+  results <- GET(yelpurl, sig, query=list(term=term, location=location, limit=limit))
+
+  extractResults = content(results)
+  resultsList=jsonlite::fromJSON(toJSON(extractResults))
+  return(data.frame(resultsList))
+}
