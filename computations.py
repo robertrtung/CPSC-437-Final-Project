138121a5e4a5c72c282674484fdc5e8f9940623b
rtung1333 <rrt23@cobra.zoo.cs.yale.edu>
2016-11-30 15:47:11 -0500

    make any amount of recommendations not just 1

--- computations.py
+++ computations.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sys
 
-def compute_recs(username):
+def compute_recs(username, numRecs):
 
 	'''
 	With that username, grab that person's favorites
@@ -31,6 +31,8 @@ def compute_recs(username):
 			notFavoriteSet.add(restaurant)
 
 	# Find restaurant with minimum distance
+	'''
+	# Outdated code kept here in case other code fails
 	minDist = sys.maxint
 	currSum = 0
 	minRest = None
@@ -43,6 +45,30 @@ def compute_recs(username):
 		if minDist > currSum:
 			minDist = currSum
 			minRest = notFav
+		currSum = 0
+	'''
+
+	# Find numRecs restaurant with minimum distance
+	minDists = []
+	minRests = []
+
+	for i in range(numRecs):
+		minDists.add(sys.maxint)
+		minRests.add(None)
+
+	for notFav in notFavoriteSet:
+		labelsNotFav = cur.execute("SELECT * FROM labels WHERE RestaurantId = %i", (notFav["RestaurantId"]))
+		for fav in favoriteSet:
+			labelsFav = cur.execute("SELECT * FROM labels WHERE RestaurantId = %i", (fav["RestaurantId"]))
+			currSum += dist(notFav,fav,labelsNotFav,labelsFav)
+		if (e = empty(minDists)) != -1:
+			minDists[e] = currSum
+			minRest[e] = notFav
+			min_sort(minDists,minRests,e)
+		if minDists[0] > currSum:
+			minDists[0] = currSum
+			minRest[0] = notFav
+			min_sort(minDists,minRests,0)
 		currSum = 0
 
 	'''
@@ -57,8 +83,6 @@ def compute_recs(username):
 
 
 def dist(rest1, rest2, rest1Labels, rest2Labels):
-	# TODO: make this distance incorporate PCA values
-	
 	physicalDist = (abs(rest1["Latitude"]-rest2["Latitude"]) ** 2 + abs(rest1["Longitude"]-rest2["Longitude"]) ** 2) ** (0.5)
 	ratingDiff = abs(rest1["Rating"]-rest2["Rating"])
 	priceDiff = abs(rest1["Price"]-rest2["Price"])
@@ -80,6 +104,21 @@ def dist(rest1, rest2, rest1Labels, rest2Labels):
 		if lab not in rest1LabelList:
 			labeldiff += 1
 
+	# TODO: make this distance incorporate PCA values
 	return 100*physicalDist + 10*ratingDiff + 5*priceDiff + labeldiff
 	
+def min_sort(dists, rests, from_index):
+	j = from_index
+	for i in range(from_index,len(dists)):
+		if dists[j] < dists[i]:
+			dists[i], dists[j] = dists[j], dists[i]
+			rests[i], rests[j] = rests[j], rests[i]
+			j = i
+		else:
+			break
 
+def empty(dists):
+	for i in range(len(dists)):
+		if dists[len(dists) - 1 - i] == None:
+			return len(dists) - 1 - i
+	return -1
