e5ca59dbd0146961205bb0744eabc96f063b1c71
jamesma618 <jamesma618@gmail.com>
2016-12-02 23:25:54 -0500

    comments

--- Data Dump SQLite.R
+++ Data Dump SQLite.R
@@ -1,9 +1,15 @@
+#import library for interfacing with SQLite
 library(sqldf)
-db <- dbConnect(SQLite(), dbname="../db/restaurantPredictions.db", flags=SQLITE_RWC)
-rest <- read.csv("../csv/restaurants.csv", as.is=TRUE)
-lab <- read.csv("../csv/labels.csv", as.is=TRUE)
+
+#establish connection with DB and import files
+db <- dbConnect(SQLite(), dbname="restaurantPredictions.db", flags=SQLITE_RWC)
+rest <- read.csv("restaurants.csv", as.is=TRUE)
+lab <- read.csv("labels.csv", as.is=TRUE)
+
+#write data to DB and check to make sure write was successful
 dbWriteTable(conn=db, name="Restaurants", value=rest, row.names=FALSE)
 dbWriteTable(conn=db, name="Labels", value=lab, row.names=FALSE)
 dbReadTable(db, "Restaurants")
 dbReadTable(db, "Labels")
+dbListTables(db)
 dbDisconnect(db)
