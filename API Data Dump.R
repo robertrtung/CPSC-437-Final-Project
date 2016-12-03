28c72ee0107ad236b184cc5d643922385c0cc1c4
jamesma618 <jamesma618@gmail.com>
2016-12-02 23:27:15 -0500

    comments for MySQL import (unused)

--- API Data Dump.R
+++ API Data Dump.R
@@ -1,11 +1,16 @@
 library(RMySQL)
 source("~/Documents/CPSC 437/Yelp Data Scrape.R")
 
+#pull cleaned restaurant data
 output <- yelp_scrape("restaurant", "new haven")
-for(i in 1:23) {
+for(i in 1:24) {
   temp <- yelp_scrape("restaurant", "new haven", offset=40*i)
+  print(names(temp))
   output <- rbind(output, temp)
+  Sys.sleep(1)
 }
+
+#insert into MySQL
 con <- dbConnect(MySQL(), 
                  user="root",
                  host="localhost",
