2edef5d22dca35b35eeeca9f3414c9c67f69413a
jamesma618 <jamesma618@gmail.com>
2016-12-02 23:27:37 -0500

    comments for Yelp API data pull

--- Yelp Data Scrape.R
+++ Yelp Data Scrape.R
@@ -1,31 +1,37 @@
-consumerKey = "JmaomcAzFt7hKPNfS98NvQ"
-consumerSecret = "4-ZNGbGD18oKjUSnL2n9Bth-QJo"
-token = "hpbsaWY87YYNRYpr2gSH6eVyo9mhH_ZJ"
-token_secret = "TZfgmUZq3gIx5vFmEwyaMrf_7yU"
 
-v3Token = "ZGCZLBa1pDe2UEMQ3JOJeGmLAetgCetLV0kb5DnlwXvXhupuWQgRqRKkfJ8VNh-R2ce1UeOT44vGm_IFBxxa4u6dA2JdLr3845yHSR4Pf5dZwtyIMWXk-Tg_5h0_WHYx"
-
+#API keys not included in script
+#import libraries
 library(httr)
 library(httpuv)
 library(jsonlite)
 
-myapp = oauth_app("YELP", key=consumerKey, secret=consumerSecret)
-sig=sign_oauth1.0(myapp, token=token,token_secret=token_secret)
-
-########
+#' 
+#' @description Function to pull and clean data from Yelp v2 and v3 Fusion APIs
+#' @param term - what you want to search for (e.g. Chinese)
+#' @param location - where you are (e.g. New Haven)
+#' @param limit - number of results to return
+#' @param offset- offset to get around API rate limit
 yelp_scrape <- function(term, location, limit=40, offset = 0) {
+  #establish oauth for Yelp v2 API
   myapp <- oauth_app("YELP", key=consumerKey, secret=consumerSecret)
   sig <- sign_oauth1.0(myapp, token=token, token_secret=token_secret)
   
+  #create query
   yelpurl <- "https://api.yelp.com/v2/search/"
   results <- GET(yelpurl, sig, query=list(term=term, location=location, limit=limit, offset=offset))
 
+  #extract pulled data
   extractResults = content(results)
   resultsList=jsonlite::fromJSON(toJSON(extractResults))
   temp <- data.frame(resultsList)
+  
+  #initalize data frame
   output <- data.frame(ID=NA, Name=unlist(temp$businesses.name), Rating=unlist(temp$businesses.rating), 
-                       Price=NA, Latitude=temp$businesses.location$coordinate$latitude, Longitude=temp$businesses.location$coordinate$longitude)
+                       Price=NA, Latitude=unlist(temp$businesses.location$coordinate$latitude), 
+                       Longitude=unlist(temp$businesses.location$coordinate$longitude))
   ids <- unlist(temp$businesses.id)
+  
+  #extract price and ID for each restaurant pulled
   for(i in 1:nrow(output)){
     output$ID[i] <- i+offset
     res <- GET(paste("https://api.yelp.com/v3/businesses/", ids[i], sep=""), add_headers(Authorization=paste("Bearer", v3Token)))
